51d98bc173359b224f5b35a6aec919f390169a28
kamil-87 <k.kamil87-87@yandex.ru>
2021-03-08 22:11:54 +0500

    added styles for nullstyle and header

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  const body = req.url === '/styles.css'
-    ? fs.readFileSync('./public/styles.css')
-    : fs.readFileSync('./public/index.html')
+  const publicPath = './public'
+
+  let body = null
+  try {
+    body = fs.readFileSync(`${publicPath}${req.url}`)
+  } catch (e) {
+    console.log(e)
+    body = fs.readFileSync(`${publicPath}/index.html`)
+  }
+
   res.end(body)
 })
 
-const port = process.env.PORT || 3000
+
+const port = process.env.PORT || 3001
 server.listen(port)
 
 console.log(`Server started on port ${port}!`)
